2586cf1ed03b96924cf34a0e1e17dc0b65438b92
quiftser <quifster@gmail.com>
2017-03-23 21:27:05 -0500

    Refactored to use the REST Ednpoints instead of relying on Google Cloud SDK

--- backend/src/main/java/com/quifster/tdc/TdcApplication.java
+++ backend/src/main/java/com/quifster/tdc/TdcApplication.java
@@ -2,11 +2,19 @@ package com.quifster.tdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TdcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TdcApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
--- backend/src/main/java/com/quifster/tdc/vision/service/GoogleVisionImplementation.java
+++ backend/src/main/java/com/quifster/tdc/vision/service/GoogleVisionImplementation.java
@@ -6,14 +6,23 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.vision.v1.Vision;
 import com.google.api.services.vision.v1.VisionScopes;
-import com.google.api.services.vision.v1.model.*;
-import com.google.common.collect.ImmutableList;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
 import com.quifster.tdc.vision.service.api.VisionService;
-import org.springframework.stereotype.Service;
-
+import com.quifster.tdc.vision.service.google.Feature;
+import com.quifster.tdc.vision.service.google.FeatureEnum;
+import com.quifster.tdc.vision.service.google.Image;
+import com.quifster.tdc.vision.service.google.ImageRequest;
+import com.quifster.tdc.vision.service.google.Request;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Google Vision Implementation.
@@ -21,30 +30,53 @@ import java.util.List;
 @Service
 public class GoogleVisionImplementation implements VisionService {
 
-    private static final String APPLICATION_NAME = "the-document-classifier";
+  private static final String APPLICATION_NAME = "the-document-classifier";
 
-    public List<EntityAnnotation> scanImage(byte[] image) throws GeneralSecurityException, IOException {
-        Vision vision = authenticate();
-        AnnotateImageRequest request = new AnnotateImageRequest()
-                .setImage(new Image().encodeContent(image))
-                .setFeatures(ImmutableList.of(
-                        new Feature().setType("TEXT_DETECTION")
-                ));
-        Vision.Images.Annotate annotate = vision.images().annotate(
-                new BatchAnnotateImagesRequest().setRequests(ImmutableList.of(request))
-        );
-        annotate.setDisableGZipContent(true);
+  private final String apiKey;
+  private final RestTemplate restTemplate;
 
-        BatchAnnotateImagesResponse batchResponse = annotate.execute();
-        AnnotateImageResponse response = batchResponse.getResponses().get(0);
-        return response.getTextAnnotations();
-    }
+  @Autowired
+  public GoogleVisionImplementation(@Value("${google.cloud.vision.apikey}") String apiKey, RestTemplate restTemplate) {
+    this.apiKey = apiKey;
+    this.restTemplate = restTemplate;
+  }
 
-    private Vision authenticate() throws IOException, GeneralSecurityException {
-        GoogleCredential credential = GoogleCredential.getApplicationDefault().createScoped(VisionScopes.all());
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
+  public List<EntityAnnotation> scanImage(byte[] inputImage) throws GeneralSecurityException, IOException {
+//        Vision vision = authenticate();
+//        AnnotateImageRequest request = new AnnotateImageRequest()
+//                .setImage(new Image().encodeContent(image))
+//                .setFeatures(ImmutableList.of(
+//                        new Feature().setType("TEXT_DETECTION")
+//                ));
+//        Vision.Image.Annotate annotate = vision.images().annotate(
+//                new BatchAnnotateImagesRequest().setRequests(ImmutableList.of(request))
+//        );
+//        annotate.setDisableGZipContent(true);
+//
+//        BatchAnnotateImagesResponse batchResponse = annotate.execute();
+//        AnnotateImageResponse response = batchResponse.getResponses().get(0);
+//        return response.getTextAnnotations();
+
+    Request request = new Request();
+    ImageRequest imageRequest = new ImageRequest();
+    com.quifster.tdc.vision.service.google.Image image = new Image();
+    image.setContent(Base64.getEncoder().encodeToString(inputImage));
+    imageRequest.setImage(image);
+
+    Feature feature = new Feature();
+    feature.setType(FeatureEnum.TEXT_DETECTION);
+    imageRequest.setFeatures(Collections.singletonList(feature));
+    request.setRequests(Collections.singletonList(imageRequest));
+
+    String result = restTemplate.postForObject("https://vision.googleapis.com/v1/images:annotate?key=" + apiKey, request, String.class);
+    return new ArrayList<>();
+  }
+
+  private Vision authenticate() throws IOException, GeneralSecurityException {
+    GoogleCredential credential = GoogleCredential.getApplicationDefault().createScoped(VisionScopes.all());
+    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+  }
 }
